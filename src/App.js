b1c494ef557d52ced2594b2ebc2f736a098ecbdc
Jason Shory <jshory@myseneca.ca>
2019-09-09 19:04:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Question from './Question';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timerOn: false,
+      time: 0,
+      displayQuestion: false,
+      gameOver: true
+    }
+  }
+
+  //set 30 second timer, subtract 1 sec. every second until timer reaches 0 and updates state
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      time: 30,
+      displayQuestion: true,
+      gameOver: false
+    });
+    this.timer = setInterval(() => {
+      const newTime = this.state.time - 1;
+      if (newTime > 0) {
+        this.setState({
+          time: newTime
+        });
+      } else {
+        clearInterval(this.timer);
+        this.setState({ timerOn: false, displayQuestion: false, gameOver: true });
+      }
+    }, 1000);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="countdown">
+          {this.state.timerOn === false &&
+            <div>
+              <h1>30-Second Trivia</h1>
+              <button className="btn" onClick={this.startTimer}>Hit Me!</button>
+            </div>
+          }
+          {this.state.displayQuestion && <h1>{this.state.time} seconds</h1>}
+          <Question status={this.state.gameOver} time={this.state.time} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Question.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import './Question.css';
+
+class Question extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            question: "",
+            correctAnswer: "",
+            shuffledAnswers: [],
+            unclickable: false,
+            clicked: false,
+            correctAnswerSelected: false,
+            gameStarted: false,
+            score: 0
+        }
+
+        this.fetchQuestion = this.fetchQuestion.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchQuestion();
+    }
+
+    //to clear previous game's score
+    static getDerivedStateFromProps(nextProps) {
+        if (nextProps.time === 30) {
+            // Question.fetchQuestion();
+            return {
+                score: 0,
+                unclickable: false,
+                clicked: false,
+                correctAnswerSelected: false
+            };
+        } else {
+            return null;
+        }
+    }
+
+    //api call to retrieve question and 4 possible answers
+    fetchQuestion() {
+        this.setState({
+            unclickable: false,
+            clicked: false,
+            correctAnswerSelected: false
+        });
+
+        fetch('https://opentdb.com/api.php?amount=1&type=multiple')
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data.results[0]);
+
+                this.setState({
+                    question: this.decodeHtml(data.results[0].question),
+                    correctAnswer: this.decodeHtml(data.results[0].correct_answer),
+                    shuffledAnswers: this.shuffle([this.decodeHtml(data.results[0].correct_answer), this.decodeHtml(data.results[0].incorrect_answers[0]), this.decodeHtml(data.results[0].incorrect_answers[1]), this.decodeHtml(data.results[0].incorrect_answers[2])])
+                });
+            })
+            .catch(console.log)
+    }
+
+    //to deal with special characters, from Open TDB api
+    decodeHtml(html) {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
+
+    //shuffle correct answer and incorrect answers
+    shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
+
+    //to be called on user click
+    checkCorrectAnswer(answer) {
+        if (this.state.correctAnswer === answer.item) {
+            this.setState({
+                unclickable: true,
+                clicked: true,
+                correctAnswerSelected: true,
+                score: this.state.score + 1,
+                gameStarted: true
+            });
+        } else {
+            this.setState({
+                unclickable: true,
+                clicked: true,
+                correctAnswerSelected: false,
+                gameStarted: true
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="question">
+                {!this.props.status &&
+                    <div>
+                        <div className="question-line">
+                            <h3 className="question-text">{this.state.question}</h3>
+                        </div>
+
+                        <div className="answers">
+                            <ul>
+                                {this.state.shuffledAnswers.map((item, i) => (
+                                    <li className={this.state.unclickable ? 'noHover' : 'hover'}
+                                        key={i}
+                                        onClick={this.state.unclickable ? null : () => {
+                                            this.checkCorrectAnswer({ item })
+                                        }}>
+                                        {item}</li>
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div className="footer">
+                            <h3 className="score">Score: {this.state.score}</h3>
+                            {this.state.clicked && <h3 className="answer-message">{this.state.correctAnswerSelected ? 'Correct!' : 'Not quite...'}</h3>}
+                            <button className="next-button" onClick={this.fetchQuestion}>Next</button>
+                        </div>
+                    </div>
+                }
+
+                {this.props.status && this.state.gameStarted && <h1 className="final-score">Final Score: {this.state.score}</h1>}
+            </div>
+        );
+    }
+}
+
+export default Question;
